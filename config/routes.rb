a511adceec0da292ac7e7175130cc91e96ca194d
Emilio Said <saidmacciseemilio@gmail.com>
2022-11-23 01:12:14 -0600

    App V1 finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/articles", to: "articles#index"
+  get "article/:id", to: "articles#article", as: "article"
+  get "articles/new", to: "articles#new", as: "new"
+  post "articles", to: "articles#create"
+  get "article/:id/edit", to: "articles#edit", as: "edit"
+  patch "article/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#delete"
 end
